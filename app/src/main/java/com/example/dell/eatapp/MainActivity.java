d957587c3731220f7f90ea6e8963ee9ce2f67dfe
mmarczuk <mateusz.marczuk@student.wat.ed.upl>
2018-02-10 16:58:45 +0100

    Dodanie widoku głównego, przycisków i modyfikacje w pliku gradle

--- app/src/main/java/com/example/dell/eatapp/MainActivity.java
+++ app/src/main/java/com/example/dell/eatapp/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.dell.eatapp;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnSignIn,btnSignUp;
+    TextView txtSlogan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSignIn = findViewById(R.id.btnSignIn);
+        btnSignUp = findViewById(R.id.btnSignUp);
+        txtSlogan = findViewById(R.id.txtSlogan);
+        Typeface typeface = Typeface.createFromAsset(getAssets(),"fonts/Nabila.ttf");
+        txtSlogan.setTypeface(typeface);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
