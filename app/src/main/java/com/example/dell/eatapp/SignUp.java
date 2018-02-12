5f8b26ed094a27ba4f48e4f03d86e7403800ae06
mmarczuk <mateusz.marczuk@student.wat.ed.upl>
2018-02-12 10:24:00 +0100

    Dodanie nowej aktywności do rejestrowania użytkownika

--- app/src/main/java/com/example/dell/eatapp/SignUp.java
+++ app/src/main/java/com/example/dell/eatapp/SignUp.java
@@ -4,8 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.dell.eatapp.Model.User;
@@ -16,45 +14,39 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.rengwuxian.materialedittext.MaterialEditText;
 
-public class SignIn extends AppCompatActivity {
+public class SignUp extends AppCompatActivity {
 
-    EditText edtPhone, edtPassword;
-    Button btnSignIn;
+    MaterialEditText edtName, edtPassword, edtPhone;
+    Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        setContentView(R.layout.activity_sign_up);
 
-        edtPhone = (MaterialEditText)findViewById(R.id.edtPhone);
+        edtName = (MaterialEditText)findViewById(R.id.edtName);
         edtPassword = (MaterialEditText)findViewById(R.id.edtPassword);
-        btnSignIn = (Button)findViewById(R.id.btnSignIn);
+        edtPhone = (MaterialEditText)findViewById(R.id.edtPhone);
+
+        btnSignUp = findViewById(R.id.btnSignUp);
 
         //Init firebase
         FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
         final DatabaseReference table_user = firebaseDatabase.getReference("User");
 
-        btnSignIn.setOnClickListener(new View.OnClickListener() {
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                ProgressBar mBar = new ProgressBar(SignIn.this);
-
-
                 table_user.addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
-
                         if (dataSnapshot.child(edtPhone.getText().toString()).exists()){
-                            //Pobranie informacji o użytkowniku
-                            User user = dataSnapshot.child(edtPhone.getText().toString()).getValue(User.class);
-                            if (user.getPassword().equals(edtPassword.getText().toString())){
-                                Toast.makeText(SignIn.this, "Sign in successful ! ", Toast.LENGTH_SHORT).show();
-                            } else {
-                                Toast.makeText(SignIn.this, "Wrong Password ! ", Toast.LENGTH_SHORT).show();
-                            }
+                            Toast.makeText(SignUp.this, "This number phone is exist", Toast.LENGTH_SHORT).show();
                         } else {
-                            Toast.makeText(SignIn.this, "User not exist in Database", Toast.LENGTH_SHORT).show();
+                            User user = new User(edtName.getText().toString(), edtPassword.getText().toString());
+                            table_user.child(edtPhone.getText().toString()).setValue(user);
+                            Toast.makeText(SignUp.this, "Sign Up successful ! ", Toast.LENGTH_SHORT).show();
+                            finish();
                         }
                     }
 
